9bfb7117e344af7e4680c846fcc64c2f0109dab5
Noah May <noahmouse2011@gmail.com>
2023-10-30 23:01:32 -0500

    Fancy wc

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from collections import Counter
+from textwrap import dedent
 
-with open("books/frankenstein.txt") as f:
-    counts = Counter(f.read().lower())
-    print(counts)
+file_path = "books/frankenstein.txt"
+with open(file_path) as f:
+    contents = f.read()
+    word_count = len(contents.split())
+
+    print(dedent(f"""
+          --- Begin report of {file_path} ---
+          {word_count} words found in the document\n"""))
+
+    counts = Counter(contents.lower())
+    for char, count in sorted(counts.items(), key=lambda x: x[1], reverse=True):
+        if not char.isalpha():
+            continue
+        print(f"The '{char}' character was found {count} times")
